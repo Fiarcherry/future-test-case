98578db897d73d81b08298b29f2bc579d2bc1281
fiarcherry <fiarchersschannel@gmail.com>
2020-09-11 20:03:01 +0300

    Создание миграции для базы данных

---
+++ database/migrations/2020_09_11_165057_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->text('author');
+            $table->text('text');
+            $table->text('time');
+            $table->text('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
